1ebafbef69a2ee7866a7bf68a3d4886e8d779395
Kris Jenkins <krisajenkins@gmail.com>
2016-10-19 23:15:06 +0100

    Works on the current directory.

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,10 +10,8 @@ import           Data.Text                  as T
 import           Data.Text.Encoding
 import           Data.Text.IO               as T
 import           System.Console.ANSI
+import           System.Directory
 import           Text.Megaparsec            as P
-
-projectPath :: Text
-projectPath = "/Users/kris/Work/WLHN/gitparse/"
 
 type Hash = Text
 
@@ -58,21 +56,30 @@ getObject hash = parse (objectParser hash) (T.unpack hash) <$> readHash hash
 
 ------------------------------------------------------------
 readHash :: Hash -> IO LBS.ByteString
-readHash hash = decompress <$> (LBS.readFile (pathForHash hash))
+readHash hash = do
+    currentDirectory <- getCurrentDirectory
+    decompress <$> LBS.readFile (pathForHash hash currentDirectory)
 
-pathForHash :: Hash -> FilePath
-pathForHash hash =
+pathForHash :: Hash -> FilePath -> FilePath
+pathForHash hash currentDirectory =
     T.unpack $
-    mconcat [projectPath, ".git/objects/", T.take 2 hash, "/", T.drop 2 hash]
+    mconcat
+        [ T.pack currentDirectory
+        , "/.git/objects/"
+        , T.take 2 hash
+        , "/"
+        , T.drop 2 hash]
 
-pathForBranch :: Text -> FilePath
-pathForBranch branchName =
-    T.unpack $ mconcat [projectPath, ".git/refs/heads/", branchName]
+pathForBranch :: Text -> FilePath -> FilePath
+pathForBranch branchName currentDirectory =
+    T.unpack $ mconcat [T.pack currentDirectory, "/.git/refs/heads/", branchName]
 
 hashForBranch :: Text -> IO Hash
-hashForBranch branchName =
-    decodeUtf8 . LBS.toStrict . LBS.take 40 <$>
-    LBS.readFile (pathForBranch branchName)
+hashForBranch branchName = do
+    currentDirectory <- getCurrentDirectory
+    let branchPath = pathForBranch branchName currentDirectory
+    print branchPath
+    decodeUtf8 . LBS.toStrict . LBS.take 40 <$> LBS.readFile branchPath
 
 ------------------------------------------------------------
 
